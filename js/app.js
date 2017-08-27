4ab439d9bda1d9434842b5ed3d8b93ff140f73d3
Dueon Kim <dueonkim89@yahoo.com>
2017-08-27 13:34:19 -0700

    search function created,

--- js/app.js
+++ js/app.js
@@ -32,6 +32,35 @@ function hideStudents() {
 	});
 }
 
+//function to see if students exist
+function findStudents(search) {
+	//if search input is empty
+	if (search === '') {
+		$('.notFound').remove();
+		var referenceNumber = parseInt($('.active').text());
+		showStudents(referenceNumber);
+		$('.pagination').show();
+	// if not empty, check through student names	
+	} else {
+		$('.notFound').remove();
+		var found = false;
+		students.each(function (index) {	
+			var studentNames = $(this).find('h3').text();
+			if (studentNames.includes(search)) {
+				$(this).removeClass('hide');
+				$('.pagination').hide();
+				found = true;
+			}
+		});		
+		//if search results not found
+		if (!found)	 {
+			var noUserFound = $(`<p class="notFound">No results for: ${search}</p>`);
+			noUserFound.insertAfter($('.student-list'));
+			$('.pagination').hide();
+		}
+	}
+}
+
 //function to show the students.
 function showStudents(paginationLink) {
 	//if not last pagination link
@@ -68,19 +97,29 @@ $(window).on('load', function () {
 });
 
 
-//delegate event listener to toggle active class on/off for the pagination links 
-//and listen for the events related to search button.
+//delegate event listener to toggle active class on/off for the pagination links & listen for the events 
+//related to search button.
 $(document).on( "click", function(event) {
 	var paginationLinks = $(event.target).parents().hasClass('pagination');
 	var unorderedList = $(event.target).parent().parent();
 	//if ancestor element is pagination div and target element is anchor.
 	if (event.target.tagName === 'A' && paginationLinks) {
+		//remove current pagination link with active class
 		unorderedList.find('.active').removeClass('active');
+		//add active class to the new pagination link
 		$(event.target).addClass('active');
+		//grab the text within the pagination link
 		var referenceNumber = parseInt($('.active').text());
 		//run function to hide
 		hideStudents();
 		//run function to show.
 		showStudents(referenceNumber);
+	//if search button	
+	} else if (event.target.id === 'searchButton') {
+		var input = $('input');
+		var searchWord = input.val();
+		hideStudents();
+		// function find students.
+		findStudents(searchWord);
 	}
 });
