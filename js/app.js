c3af83f66a659af25a459f84439f4ccb9c743234
Dueon Kim <dueonkim89@yahoo.com>
2017-08-27 00:01:46 -0700

    pagination link functionality created

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var students = $('.student-list').children();
 var numberOfStudents = $('.student-list').children().length;
 
 var numberOfPaginationLinks = Math.ceil(numberOfStudents/10);
@@ -24,37 +25,62 @@ function createSearchDiv() {
 	$('.student-search').append(searchField, searchButton);
 }
 
-//function to hide the proper students
+//function to hide all the students
 function hideStudents() {
-	var referenceNumber = parseInt($('.active').text());
-	//run a for loop. 
-	// from the refernceNumber to the numberOfStudents
-	// add a class of hide to all the apprporiate li within student-list
-	
+	students.each(function (index) {
+		$(this).addClass('hide');
+	});
 }
 
 //function to show the students.
-
-
-
+function showStudents(paginationLink) {
+	//if not last pagination link
+	if (numberOfStudents - paginationLink * 10 > 0) {
+		students.each(function (index) {
+			if (paginationLink * 10 - 10 <= index && index < paginationLink * 10) {
+				$(this).removeClass('hide');
+			}
+		});		
+	// else it is last pagination link	
+	} else {
+		students.each(function (index) {
+			if ((paginationLink - 1) * 10 <= index && index < numberOfStudents ) {
+				$(this).removeClass('hide');
+			}
+		});			
+	}
+}
 
 //when window loads. Create pagination links 
 $(window).on('load', function () {
 	//create the search section
-	//createSearchDiv();
+	createSearchDiv();
 	//create the pagination links
-	//createPaginationLinks();
+	createPaginationLinks();
 	//give first link the class of active.
-	//$( ".pagination ul li:nth-child(1)" ).find('a').addClass('active');
+	$( ".pagination ul li:nth-child(1)" ).find('a').addClass('active');
 	//toggle the .hide class for appropriate students
-
-	
+	students.each(function (index) {
+		if (index >= 10) {
+			$(this).addClass('hide');
+		}
+	});
 });
-
 
 
 //delegate event listener to toggle active class on/off for the pagination links 
 //and listen for the events related to search button.
 $(document).on( "click", function(event) {
-	
+	var paginationLinks = $(event.target).parents().hasClass('pagination');
+	var unorderedList = $(event.target).parent().parent();
+	//if ancestor element is pagination div and target element is anchor.
+	if (event.target.tagName === 'A' && paginationLinks) {
+		unorderedList.find('.active').removeClass('active');
+		$(event.target).addClass('active');
+		var referenceNumber = parseInt($('.active').text());
+		//run function to hide
+		hideStudents();
+		//run function to show.
+		showStudents(referenceNumber);
+	}
 });
