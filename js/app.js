ca319abb3642fff8c57e64853ef5314540ec1353
Dueon Kim <dueonkim89@yahoo.com>
2017-08-27 21:58:53 -0700

    modify search func, add pagination based on search results

--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@ var students = $('.student-list').children();
 var numberOfStudents = $('.student-list').children().length;
 
 var numberOfPaginationLinks = Math.ceil(numberOfStudents/10);
+var searchResults = [];
 
 //create pagination links based off the var numberOfPaginationLinks
-function createPaginationLinks() {
+function createPaginationLinks(length) {
 	var paginationDiv = $(`<div class="pagination"></div>`);
 	paginationDiv.insertAfter(".student-list");
 	var unorderedList = $(`<ul></ul>`);
 	$('.pagination').append(unorderedList);
-	for (i=1; i <=numberOfPaginationLinks; i++) {
+	for (i=1; i <=length; i++) {
 		var listItem = $(`<li><a href="#">${i}</a></li>`);
 		unorderedList.append(listItem);
 	}
@@ -32,51 +33,83 @@ function hideStudents() {
 	});
 }
 
+// default settings function
+function defaultSettings() {
+	//create the pagination links
+	createPaginationLinks(numberOfPaginationLinks);
+	//give first link the class of active.
+	$( ".pagination ul li:nth-child(1)" ).find('a').addClass('active');
+	//toggle the .hide class for appropriate students
+}
+
 //function to see if students exist
 function findStudents(search) {
+	//remove pagination links
+	$('.pagination').remove();
 	//if search input is empty
 	if (search === '') {
 		$('.notFound').remove();
-		var referenceNumber = parseInt($('.active').text());
-		showStudents(referenceNumber);
-		$('.pagination').show();
+		defaultSettings();
+		students.each(function (index) {
+			searchResults.push($(this));
+			if (index <= 9) {
+				$(this).removeClass('hide');
+			}
+		});		
 	// if not empty, check through student names	
 	} else {
+		//set found variable to false
+		var found = false;		
 		$('.notFound').remove();
-		var found = false;
 		students.each(function (index) {	
 			var studentNames = $(this).find('h3').text();
 			if (studentNames.includes(search)) {
-				$(this).removeClass('hide');
-				$('.pagination').hide();
+				searchResults.push($(this));
 				found = true;
 			}
-		});		
-		//if search results not found
-		if (!found)	 {
+		});	
+		//if name is not found.
+		if (!found) {
 			var noUserFound = $(`<p class="notFound">No results for: ${search}</p>`);
 			noUserFound.insertAfter($('.student-list'));
-			$('.pagination').hide();
-		}
-	}
+			$('.pagination').remove();	
+		// if search result is less than or equal to 10.		
+		} else if (searchResults.length <= 10) {
+			//loop thru the searchResults array
+			for (i = 0; i<searchResults.length; i++ ) {
+				//remove the class hide
+				searchResults[i].removeClass('hide');
+			}
+		} else if (searchResults.length > 10) {
+			var numberOfLinks = Math.ceil(searchResults.length/10);
+			//create pagination links based off length
+			createPaginationLinks(numberOfLinks);
+			//set first link as active class
+			$( ".pagination ul li:nth-child(1)" ).find('a').addClass('active');
+			//show first 10, etc.
+			showStudents(searchResults.length, 1);
+		} 
+	}	
 }
 
 //function to show the students.
-function showStudents(paginationLink) {
+function showStudents(length, paginationLink) {
 	//if not last pagination link
-	if (numberOfStudents - paginationLink * 10 > 0) {
-		students.each(function (index) {
-			if (paginationLink * 10 - 10 <= index && index < paginationLink * 10) {
-				$(this).removeClass('hide');
+	if (length - paginationLink * 10 > 0) {
+		//refernce the array instead
+		for (i = 0; i<searchResults.length; i++) {
+			if (paginationLink * 10 - 10 <= i && i < paginationLink * 10) {
+				searchResults[i].removeClass('hide');
 			}
-		});		
+		}	
 	// else it is last pagination link	
 	} else {
-		students.each(function (index) {
-			if ((paginationLink - 1) * 10 <= index && index < numberOfStudents ) {
-				$(this).removeClass('hide');
+		//refernce the array instead
+		for (i = 0; i<searchResults.length; i++) {
+			if ((paginationLink - 1) * 10 <= i && i < length ) {
+				searchResults[i].removeClass('hide');
 			}
-		});			
+		}		
 	}
 }
 
@@ -84,16 +117,14 @@ function showStudents(paginationLink) {
 $(window).on('load', function () {
 	//create the search section
 	createSearchDiv();
-	//create the pagination links
-	createPaginationLinks();
-	//give first link the class of active.
-	$( ".pagination ul li:nth-child(1)" ).find('a').addClass('active');
-	//toggle the .hide class for appropriate students
+	//invoke default settings function
+	defaultSettings();
 	students.each(function (index) {
+		searchResults.push($(this));
 		if (index >= 10) {
 			$(this).addClass('hide');
 		}
-	});
+	});	
 });
 
 
@@ -112,10 +143,12 @@ $(document).on( "click", function(event) {
 		var referenceNumber = parseInt($('.active').text());
 		//run function to hide
 		hideStudents();
-		//run function to show.
-		showStudents(referenceNumber);
+		//run function to show based off length
+		showStudents(searchResults.length, referenceNumber);
 	//if search button	
 	} else if (event.target.id === 'searchButton') {
+		//reset array to 0 length
+		searchResults.length = 0;		
 		var input = $('input');
 		var searchWord = input.val();
 		hideStudents();
